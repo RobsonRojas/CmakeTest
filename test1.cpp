1c688e6a63897bc37e10e2b52c2357c58d10870f
Robson Rojas Andrade <roj@certi.org.br>
2017-07-10 17:32:23 -0300

    Includes and versions

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,10 @@
 // a simple program
 #include <stdio.h>
 #include <stdlib.h>
+#include "TutorialConfig.h"
 
 int main (int argc, char *argv[])
 {
-	printf("Hello World");
+	printf("Hello World\n");
+	printf("%s version: %d.%d\n", argv[0], Tutorial_VERSION_MAJOR, Tutorial_VERSION_MINOR);
 }
