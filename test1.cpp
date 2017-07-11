cac13927ca5aa96503c3fa5e5ba2e237abd7266a
Robson Rojas Andrade <roj@certi.org.br>
2017-07-11 14:20:44 -0300

    STEP2

--- test1.cpp
+++ test1.cpp
@@ -1,10 +1,34 @@
 // a simple program
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "TutorialConfig.h"
+
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
 
 int main (int argc, char *argv[])
 {
-	printf("Hello World\n");
-	printf("%s version: %d.%d\n", argv[0], Tutorial_VERSION_MAJOR, Tutorial_VERSION_MINOR);
+  if (argc < 2)
+    {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+
+#ifdef USE_MYMATH
+	double outputValue = mysqrt(inputValue);
+#else
+	double outputValue = sqrt(inputValue);
+#endif
+  
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
 }
